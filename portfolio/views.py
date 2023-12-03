014f35098a280cf05d74954875c9d4ab15c97b0c
jhans11 <deibyok@gmail.com>
2023-12-03 12:27:03 -0500

    agregando proyecto

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Project
+
+
+def home(request):
+    projects = Project.objects.all()
+    
+    return render(request, 'home.html', {'projects': projects})
